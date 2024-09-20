ee83341408deee77f66d8004e26d0567c89f1383
Cone Perez <oscarerik@gmail.com>
2024-09-20 14:04:14 -0600

    cambiar codigo para conectar a la base de datos

--- base.py
+++ base.py
@@ -1,10 +1,21 @@
 # base.py
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-db_string_con = os.environ.get("DB_STRING_CON")
-engine = create_engine(db_string_con)
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD")
+db_host = os.environ.get("DB_HOST")
+
+connection_string = URL.create(
+    'postgresql',
+    username = db_user,
+    password= db_password,
+    host=db_host,
+    database='koyebdb',
+)
+
+engine = create_engine(connection_string)
 Session = sessionmaker(bind=engine, expire_on_commit=False)
 Base = declarative_base()
